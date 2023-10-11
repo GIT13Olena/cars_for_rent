100df5ed764981c15d88654f172d77428a315a52
GIT13Olena <girnyak.1olena@gmail.com>
2023-10-11 19:14:59 +0300

    css HomePage and photo replacement

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -12,12 +12,16 @@ function HomePage() {
        <p className={cssHome.textOrCompany}>"RentCar is a modern car rental company designed for your convenience. 
         On our website, you can easily find and book a car that meets your needs. 
         Our mission is to provide reliable and comfortable transportation for your 
-        trip. The website is designed with one main goal in mind - to make the car 
-        rental process as easy and convenient as possible for you. So go to the catalog 
-        and choose a car as soon as possible.</p>
+        trip.</p>
         <img src={carsImg} alt="cars" className={cssHome.imgCars}/>
       </div>
+      <div className={cssHome.blockInfoAndImg}>
       <img src={companyCars} alt="Company Cars" className={cssHome.companyImg}/>
+      <p className={cssHome.textOrCompany}>The website is designed with one main goal in mind - to make the car 
+        rental process as easy and convenient as possible for you. So go to the catalog 
+        and choose a car as soon as possible.</p>
+      </div>
+      
     </>
   );
 }
